1b608739c14ea3bbb5e3e3b351d80ac861837919
Stephen P. Schaefer <sschaefer@acm.org>
2011-09-14 08:10:09 -0400

    Getting EDoc and -spec lines working; initially, for miniman_genserver_test.erl

--- src/minimal_genserver_test.erl
+++ src/minimal_genserver_test.erl
@@ -4,18 +4,14 @@
 %%% @copyright Stephen P. Schaefer
 %%% @doc Eunit tests for minimal genserver behavior implementation provided in
 %%%      the "OTP in Action" book by Logan, Merit and Carlson.
-%%% @end
-%%%------------------------------------------------------------------------------
--module(minimal_genserver_test).
-
-%%%==============================================================================
-%%% Setup
-%%% @doc To test a different genserver behavior implementation, one nees to
+%%%
+%%%      To test a different genserver behavior implementation, one nees to
 %%%      change the value of MODULE_TESTED, and the name of the include file.
 %%%      A change in the eunit test version must be accomodated in the other
 %%%      include file name.
 %%% @end
 %%%==============================================================================
+-module(minimal_genserver_test).
 
 -include("eunit-2.1.7/include/eunit.hrl").
 
@@ -23,39 +19,47 @@
 -include("minimal_genserver.hrl").
 
 -export([]).
+-ifndef(TEST).
+-export([
+	 init_test/0,
+	 handle_call_test/0,
+	 handle_cast_test/0,
+	 handle_info_test/0,
+	 terminate_test/0,
+	 code_change_test/0
+	]).
+-endif.
 
 %%%==============================================================================
 %%% Tests
 %%%==============================================================================
 
-%% @spec init_test() -> {ok, #state{}}
-%% @end
 %% @doc test of init/1
-%% @end
+-spec init_test() -> tuple(ok, #state{}).
 init_test() ->
     {ok, #state{}} = ?MODULE_TESTED:init([]).
 
-%% @spec handle_call_test -> {reply, ok, preserved_state}
 %% @doc test of handle_call/3
+-spec handle_call_test() -> tuple(reply, ok, preserved_state).
 handle_call_test() ->
     {reply, ok, preserved_state} = ?MODULE_TESTED:handle_call(discarded_request, discarded_from, preserved_state).
 
-%% @spec handle_cast_test -> {noreply, preserved_state}
 %% @doc test of handle_cast/2
+-spec handle_cast_test() -> {noreply, preserved_state}.
 handle_cast_test() ->
     {noreply, preserved_state} = ?MODULE_TESTED:handle_cast(discarded_msg, preserved_state).
 
-%% @spec handle_info_test -> {noreply, preserved_state}
 %% @doc test of handle_info/2
+-spec handle_info_test() -> {noreply, preserved_state}.
 handle_info_test() ->
     {noreply, preserved_state} = ?MODULE_TESTED:handle_info(discarded_info, preserved_state).
 
-%% @spec terminate_test -> ok
 %% @doc test of terminate/2
+-spec terminate_test() -> ok.
 terminate_test() ->
     ok = ?MODULE_TESTED:terminate(discarded_reason, discarded_state).
 
-%% @spec code_change_test -> {ok, preserved_state}
 %% @doc test of code_change/3
+-spec code_change_test() -> {ok, preserved_state}.
 code_change_test() ->
     {ok, preserved_state} = ?MODULE_TESTED:code_change(old_version, preserved_state, extra).
